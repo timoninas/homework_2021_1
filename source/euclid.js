d51d73efb8649b23dd7139c4e72f3bdbc1cb5731
Timonin Anton <dotruger.37@gmail.com>
2021-02-08 23:55:52 +0300

    Remove convert rest operator to array

--- source/euclid.js
+++ source/euclid.js
@@ -6,17 +6,15 @@
  * @param {...number} numbers - Переменное число параметров.
  */
 const euclid = (...numbers) => {
-    let arrayOfNumbers = Array.from(numbers)
-
     // Проверка на корректность входных данных
-    arrayOfNumbers.forEach((item) => {
+    numbers.forEach((item) => {
         if (typeof item !== 'number') {
             throw TypeError('Incorrect input')
         }
     })
 
     // Алгоритм Евклида
-    return arrayOfNumbers.reduce((result, currentValue) => {
+    return numbers.reduce((result, currentValue) => {
         let a = result;
         let b = currentValue;
         while (a !== b) {
@@ -28,6 +26,6 @@ const euclid = (...numbers) => {
             b = b - a;
         }
         return a
-    }, arrayOfNumbers[0] === undefined ? 0: arrayOfNumbers[0] )
+    }, numbers[0] === undefined ? 0: numbers[0] )
 }
 
